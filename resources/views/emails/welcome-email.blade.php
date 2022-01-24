2dc1c6b21d1655c31aa29f289842c299c68af8ae
Tunahan Kuzucu <tunahankuzucuu@gmail.com>
2022-01-24 15:05:13 +0100

    finalized the project

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+Welcome to freeCodeGram!
+
+This is a community of fellow developers and we love that you have joined us.
+
+All the best,
+Tunahan
+
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,37 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-full max-w-5xl mx-auto bg-white grid grid-flow-row ">
+<div class="w-full max-w-5xl mx-auto bg-white flex flex-col gap-4 ">
         @foreach($posts as $post)
-                <img class="w-3/6  border-2 " src="/storage/{{ $post->image }}" alt="">
-        
-             <div class="flex justify-start items-start border-t-2 border-b-2 border-r-2 w-3/6">
-               <div>
-                  <div class="flex flex-row justify-start items-center gap-4 mx-2 my-2 w-full">
-                    <div class="w-max">
+             <div class="flex flex-row justify-center items-start ">
+               <div class="w-full ">
+                  <div class="w-full flex flex-row justify-center items-start   w-full gap-2">
+                   <div class="w-full">
+                    <img class="w-full  border-2 " src="/storage/{{ $post->image }}" alt="">
+                   </div>
+                    <div class="w-1/4 mt-4">
                        <img class="w-24 rounded-full" src="{{ $post->user->profile->profileImage() }}" alt="">
                      </div>
-                     <div class="w-max">
+                     <div class="w-1/4 mt-4">
                        <a href="/profile/{{ $post->user->id }}" class="font-bold"> {{ $post->user->username }}</a>
                      </div>
-                     <div>
-                        <form action="/follow/{{$post->user->id}}"  method="post"> 
-                         @csrf
-                            @if (auth()->user()->following->contains($post->user->id) == true) 
-                            <button value="Unfollow" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4 pt-2">Unfollow</button>
-                            @else
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4 pt-2">Follow</button>
-                            @endif
-                         </form>
-                      </div>
-                   </div>
-                   <hr class="ml-2">
-                   <div class="ml-2 mt-4 text-left w-full flex gap-2">
-                       <a href="/profile/{{ $post->user->id }}" class="font-bold"> {{ $post->user->username }}</a>
+                     
+                      <div class="ml-2 mt-4 text-left w-full flex gap-2">
+                       
                        <p>{{ $post->caption }}</p>
-                   </div>
+                      </div>
+                  </div>
                </div>
-              </div>
-        @endforeach  
+             </div>
+        @endforeach
+        <div>
+         {{ $posts->links()}}
+        </div>  
 </div>
 @endsection
